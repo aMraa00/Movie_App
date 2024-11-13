22044072734d810cb0cea1a0c1488438a83e528e
aMraa <you@example.com>
2024-11-14 04:39:54 +0800

    feat: Folder structure

---
+++ lib/widgets/movie_card.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class MovieCard extends StatelessWidget {
+  const MovieCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
